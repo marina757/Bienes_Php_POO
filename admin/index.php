7860810548bdf7d8352c81746c7df1092754b7af
marina <marinamarrina@hotmail.com>
2021-12-10 12:26:03 +0100

    venditori del db

--- admin/index.php
+++ admin/index.php
@@ -16,6 +16,7 @@
     // var_dump($resultado);
 
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        debuguear($_POST);
         $id = $_POST['id'];
         $id = filter_var($id, FILTER_VALIDATE_INT);
     
@@ -45,6 +46,7 @@
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
 
+        <h2>Propiedades</h2>
         <table class="propiedades">
             <thead>
                 <tr>
@@ -76,12 +78,40 @@
                 <?php endforeach; ?>
             </tbody>
         </table>
+
+        <h2>Vendedores</h2>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Telefono</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody><!--MOSTRAR LOS RESULTADOS-->
+                <?php foreach ($vendedores as $vendedor): ?>                
+                <tr>
+                    <td><?php echo $vendedor->id; ?></td>
+                    <td><?php echo $vendedor->nombre . " " . $vendedor->apellido; ?></td>
+                    <td><?php echo $vendedor->telefono; ?></td>
+                    <td>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>">
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        
+                        <a href="admin/vendedores/actualizar.php?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
     </main>
 
 <?php
-
-    //CERRAR LA CONEXION(OPCIONAL)
-    mysqli_close($db);
-
     incluirTemplate('footer');  
 ?>    
